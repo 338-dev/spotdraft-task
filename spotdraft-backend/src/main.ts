d4dd593f5e61c613241d35578f8cc84a4d528b3e
sahdevrai <sahdev.rai@bitcs.in>
2023-07-10 17:30:09 +0530

    deploy

--- spotdraft-backend/src/main.ts
+++ spotdraft-backend/src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: 'https://pdf-keeper.netlify.app',
     credentials: true,
   });
 
